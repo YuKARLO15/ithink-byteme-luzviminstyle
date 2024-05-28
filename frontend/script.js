6214a71758360bf0718efc55a6de1187315d0ca3
YuKARLO15 <karlorobert15+programming@gmail.com>
2024-05-28 12:49:00 +0800

    Added new features
    Bug: Active Labels on Nav and Gallery not working properly

--- frontend/script.js
+++ frontend/script.js
@@ -5,3 +5,40 @@ var typed = new Typed(".multiple-text", {
     backDelay: 1000,
     loop: true
 })
+
+window.addEventListener('scroll', function() {
+    // Get all the sections and navbar links
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.nav-link');
+  
+    // Loop through the sections and navbar links
+    sections.forEach((section, i) => {
+      const sectionTop = section.offsetTop;
+    //   const sectionHeight = section.offsetHeight;
+  
+      // Check if the user has scrolled to this section
+        if (window.pageYOffset >= sectionTop) {
+        // Remove the "active" class from all navbar links
+        navLinks.forEach(link => link.classList.remove('active'));
+  
+        // Add the "active" class to the correct navbar link
+        navLinks[i].classList.add('active');
+      }
+    });
+  });
+
+const radioButtons = document.querySelectorAll('input[type="radio"]');
+
+radioButtons.forEach(radioButton => {
+  radioButton.addEventListener('change', function() {
+    const clickedRadioButtonId = this.id;
+    const correspondingLabel = document.querySelector(`.top-content label[for="${clickedRadioButtonId}"]`);
+    
+    // Remove active class from all labels
+    const labels = document.querySelectorAll('.top-content label');
+    labels.forEach(label => label.classList.remove('active'));
+    
+    // Add active class to the clicked label
+    correspondingLabel.classList.add('active');
+  });
+});
