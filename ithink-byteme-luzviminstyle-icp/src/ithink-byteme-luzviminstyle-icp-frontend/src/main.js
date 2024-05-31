3b487005dc9fe29b400018e3611998b2833aad59
YuKARLO15 <karlorobert15+programming@gmail.com>
2024-05-31 13:06:51 +0800

    Configured files for deployment
    Changed codes and paths
    
    Changed some policies
    
    Succesfully deployed canister on dfx mainnet

---
+++ ithink-byteme-luzviminstyle-icp/src/ithink-byteme-luzviminstyle-icp-frontend/src/main.js
@@ -0,0 +1,12 @@
+//import React from 'react';
+//import ReactDOM from 'react-dom/client';
+//import App from './App';
+//import './index.scss';
+import 'https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js';
+import './script.js';
+
+//ReactDOM.createRoot(document.getElementById('root')).render(
+  //<React.StrictMode>
+    //<App />
+  //</React.StrictMode>,
+//);
---
+++ ithink-byteme-luzviminstyle-icp/src/ithink-byteme-luzviminstyle-icp-frontend/src/script.js
@@ -0,0 +1,47 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Gowns and Suits"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+window.addEventListener('scroll', function() {
+    // Get all the sections and navbar links
+    const sections = document.getElementsByClassName('section');
+    const navLinks = document.querySelectorAll('.nav-link');
+    
+    // Loop through the sections and navbar links
+    Array.from(sections).forEach((section, i) => {
+      const sectionTop = section.offsetTop;
+    //   const sectionHeight = section.offsetHeight;
+  
+      // Check if the user has scrolled to this section
+        if (window.scrollY >= sectionTop) {
+        // Remove the "active" class from all navbar links
+        navLinks.forEach(link => {link.classList.remove('active')});
+  
+        // Add the "active" class to the correct navbar link
+        navLinks[i].classList.add('active');
+      }
+    });
+  });
+
+const radioButtons = document.querySelectorAll('input[type="radio"]');
+
+radioButtons.forEach(radioButton => {
+  radioButton.addEventListener('change', function() {
+    const clickedRadioButtonId = this.id;
+    const correspondingLabel = document.querySelector(`.top-content label[for="${clickedRadioButtonId}"]`);
+    
+    // Remove active class from all labels
+    const labels = document.querySelectorAll('.top-content label');
+    labels.forEach(label => label.classList.remove('active'));
+    
+    // Add active class to the clicked label
+    correspondingLabel.classList.add('active');
+    
+    // Change label color of selected radio button
+    //correspondingLabel.style.color = 'black'; // Replace 'red' with desired color
+  });
+});
